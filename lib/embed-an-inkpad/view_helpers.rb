3a5e68749d03aa554fd6221a15af09c52a9aa376
Lars Kluge <l@larskluge.com>
2015-05-15 19:53:18 +0100

    Load requested inkpad as md for now & render in pre tag

--- lib/embed-an-inkpad/view_helpers.rb
+++ lib/embed-an-inkpad/view_helpers.rb
@@ -1,7 +1,19 @@
 module EmbedAnInkpad
   module ViewHelpers
     def embed_an_inkpad inkpad_id
-      'Foo'
+
+
+      uri = URI("https://www.inkpad.io/#{inkpad_id}.md")
+
+      Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+        request = Net::HTTP::Get.new uri
+
+        response = http.request request # Net::HTTPResponse object
+
+        content_tag :pre do
+          response.body
+        end
+      end
     end
   end
 end
