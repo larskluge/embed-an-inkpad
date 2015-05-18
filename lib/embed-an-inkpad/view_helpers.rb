ad31398f224497a4e374eb603fccdfcb9995fedb
Lars Kluge <l@larskluge.com>
2015-05-18 14:59:17 +0100

    Enable HTML embedding

--- lib/embed-an-inkpad/view_helpers.rb
+++ lib/embed-an-inkpad/view_helpers.rb
@@ -1,20 +1,18 @@
 module EmbedAnInkpad
   module ViewHelpers
+
     def embed_an_inkpad inkpad_id
-
-
-      uri = URI("https://www.inkpad.io/#{inkpad_id}.md")
+      uri = URI("https://www.inkpad.io/#{inkpad_id}")
 
       Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
         request = Net::HTTP::Get.new uri
+        request['Accept'] = 'text/html;vanilla'
 
-        response = http.request request # Net::HTTPResponse object
-
-        content_tag :pre do
-          response.body
-        end
+        response = http.request request
+        response.body.html_safe
       end
     end
+
   end
 end
 
